a2b7109a4e2af0a004608d462e9d76c6bd720ed7
Cyrille <cyrille.rieau@gmail.com>
2017-08-22 10:37:54 +0200

    Commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-module.exports = function fetch(url) {
+var XMLHttpRequest = request("xmlhttprequest").XMLHttpRequest;
+
+function fetch(url) {
     return new Promise(function(resolve, reject) {
+
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = function() {
@@ -20,3 +23,30 @@ fetch("https://dog.ceo/api/breeds/list/all").then(function(response) {
 }).catch(function(error) {
     console.error(error);
 });
+module.exports = fetch;
+
+
+/*function modif() {
+    var http = require('http');
+
+    var options = {
+        host: 'www.dog.ceo',
+        path: '/api/breeds/list/all',
+    };
+
+    callback = function(response) {
+        var str = ''
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function() {
+            console.log(str);
+        });
+    }
+
+    var req = http.request(options, callback);
+    req.end();
+}
+
+exports.modif = modif;*/
